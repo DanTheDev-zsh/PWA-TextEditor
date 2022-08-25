9160398826b703cc06f9544bd37583fc4628641c
DanTheDev-zsh <XuDanDev@gmail.com>
2022-08-25 15:11:21 -0700

    fixed most bugs

---
+++ Develop/client/src-sw.js
@@ -0,0 +1,46 @@
+const { registerRoute } = require('workbox-routing');
+const { StaleWhileRevalidate, CacheFirst } = require('workbox-strategies');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+
+const { ExpirationPlugin } = require('workbox-expiration');
+
+const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+const pageCache = new CacheFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+warmStrategyCache({
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
+});
+
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// TODO: Implement asset caching
+registerRoute(
+    ({ request }) => request.destination === 'image',
+    new CacheFirst({
+        cacheName: 'my-image-cache',
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+            new ExpirationPlugin({
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+            }),
+        ],
+    })
+);
